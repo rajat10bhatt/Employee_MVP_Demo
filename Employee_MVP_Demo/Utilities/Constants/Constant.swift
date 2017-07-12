a63d23dd0e7c24283d073d7783a8929bf51b7508
Rajat Bhatt <rajatbhatt@ansh100.local>
2017-07-12 17:19:17 +0530

    second commit

---
+++ Employee_MVP_Demo/Utilities/Constants/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  AppName
+//
+//  Created by Amol Chaudhari on 07/05/17.
+//  Copyright © 2017 Amol Chaudhari. All rights reserved.
+//
+
+import UIKit
+
+class Constant: NSObject {
+
+}
