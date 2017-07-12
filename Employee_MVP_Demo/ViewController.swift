5b5f8193fdbd5badd12757ed58df560d26659e5e
Rajat Bhatt <rajatbhatt@ansh100.local>
2017-07-12 10:42:55 +0530

    Initial Commit

---
+++ Employee_MVP_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Employee_MVP_Demo
+//
+//  Created by Rajat Bhatt on 12/07/17.
+//  Copyright © 2017 Rajat Bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
